811a27e97ec79122a501ce5dfdcc313be260d72b
José Ricardo <zevatron@zevatron.com.br>
2018-02-05 13:00:47 -0300

    #4 - scaffold  Person /relacionamento bi-direcional entre Person e User.
    #4 - Inclusão do scaffold para Person e criação do relacionamento bi-direcional entre Person e User.

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180203190739) do
+ActiveRecord::Schema.define(version: 20180205143322) do
+
+  create_table "people", force: :cascade do |t|
+    t.string "nome"
+    t.string "telefone"
+    t.date "dataNascimento"
+    t.integer "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_people_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
